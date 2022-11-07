bc81a15b9f1881240b790435cda2adb23712ce16
thisisdishang <dishangkumarrana@gmail.com>
2022-11-07 17:36:00 +0530

    PHP Website

---
+++ FeedbackSystem/feedback.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Student Feedback System</title>
+	<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="topHeader">
+	RANA CORPORATION ®<br />
+    <span class="tag">STUDENT FEEDBACK SYSTEM</span>
+</div>
+<br>
+<br>
+<br>
+<br>
+<div id="content" align="center">
+<br>
+<br>
+<span class="SubHead">Student Feedback Step I</span>
+<form method="post" action="feedback_step_2.php" >
+<div id="table"> 
+    <div class="tr">
+		<div class="td">
+        	<label>Roll No : </label>
+        </div>
+        <div class="td">
+			<select name="roll" required>
+            <option value="NA" disabled selected> - - Select Roll No. - -</option>
+            <?php
+			for($x=1;$x<=500;$x++)
+			{
+				?>
+                <option value="<?php echo $x;?>"><?php echo $x;?></option>
+                <?php } ?>
+                </select>
+        </div>
+    </div>
+</div>		
+        <div class="tdd">
+        	<input type="button" onClick="window.location='index.php'" value="BACK">&nbsp;&nbsp;&nbsp;&nbsp;<input type="submit" value="NEXT" />
+        </div>    
+    <br>
+</div>
+</form>
+<br>
+</body>
+</html>
---
+++ FeedbackSystem/home.php
@@ -0,0 +1,47 @@
+<?php
+include("configASL.php");
+session_start();
+if(!isset($_SESSION['aid']))
+{
+	header("location:index.php");
+}
+$aid=$_SESSION['aid'];
+$x=mysqli_query($al,"select * from admin where aid='$aid'");
+$y=mysqli_fetch_array($x);
+$name=$y['name'];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Student Feedback System</title>
+	<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="topHeader">
+	RANA CORPORATION ®<br />
+    <span class="tag">STUDENT FEEDBACK SYSTEM</span>
+</div>
+<br>
+<br>
+<br>
+<br>
+<div id="content" align="center">
+<br>
+<br>
+<span class="SubHead">Welcome Admin <?php echo $name;?></span>
+<br>
+<br>
+<a href="feeds.php" class="button">Feedback</a>
+<a href="manageFaculty.php" class="button">Manage Faculty</a>
+<br>
+<br>
+<br>
+<a href="changePass.php" class="button">Change Password</a>
+<a href="logout.php" class="button">Logout</a>
+<br>
+<br>
+</div>
+</body>
+</html>
+
